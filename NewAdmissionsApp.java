159d4756dfa396297b58149f1127f662f8af877e
Praful Shrestha <81242171+Praful120@users.noreply.github.com>
2023-04-07 13:33:42 +0545

    Add files via upload

---
+++ NewAdmissionsApp.java
@@ -0,0 +1,78 @@
+package exam;
+
+import BookStore.Book;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NewAdmissionsApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student student1 = new Student();
+        Student student2 = new Student();
+        ArrayList<Student> students = new ArrayList<>();
+
+
+        System.out.println("Enter details of Student ");
+
+        System.out.print("Enter student name : ");
+        student1.setStudentName(sc.nextLine());
+        System.out.print("Enter past Grade (CGPA) in credit hours : ");
+        student1.setPastGrade(sc.nextInt());
+        sc.nextLine();      //garbage collecter
+        System.out.print("Enter admissionFee : ");
+        student1.setAdmissionFee(sc.nextInt());
+        sc.nextLine();
+        System.out.print("Enter department name : ");
+        student1.setDeptName(sc.next());
+        System.out.print("Enter course name : ");
+        student1.setCourseName(sc.next());
+        students.add(student1);
+
+        sc.nextLine();
+        System.out.println("Enter details of next student ");
+
+        System.out.print("Enter student name : ");
+        student2.setStudentName(sc.nextLine());
+        System.out.print("Enter past Grade (CGPA) in credit hours : ");
+        student2.setPastGrade(sc.nextInt());
+        sc.nextLine();      //garbage collecter
+        System.out.print("Enter admissionFee : ");
+        student2.setAdmissionFee(sc.nextInt());
+        sc.nextLine();
+        System.out.print("Enter department name : ");
+        student2.setDeptName(sc.next());
+        System.out.print("Enter course name : ");
+        student2.setCourseName(sc.next());
+        students.add(student2);
+
+        //displaying all details (without discount)
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+
+        //generating receipts...
+        for (Student student : students){
+            int admissionFee = student.getAdmissionFee();
+            int pastGrade = student.getPastGrade();
+            int discount = 0;
+
+            if (pastGrade > 350 ){
+                discount = 1500;
+            }
+
+            int totalFee = admissionFee - discount;
+
+            student.setAdmissionFee(totalFee);
+
+            System.out.println(student.toString());
+
+        }
+
+
+
+    }
+
+
+}
---
+++ Student.java
@@ -0,0 +1,60 @@
+package exam;
+
+public class Student {
+    private String studentName;
+    private String deptName;
+    private String courseName;
+    private int pastGrade;
+    private int admissionFee;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getPastGrade() {
+        return pastGrade;
+    }
+
+    public void setPastGrade(int pastGrade) {
+        this.pastGrade = pastGrade;
+    }
+
+    public int getAdmissionFee() {
+        return admissionFee;
+    }
+
+    public void setAdmissionFee(int admissionFee) {
+        this.admissionFee = admissionFee;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", deptName='" + deptName + '\'' +
+                ", courseName='" + courseName + '\'' +
+                ", pastGrade=" + pastGrade +
+                ", admissionFee=" + admissionFee +
+                '}';
+    }
+}
